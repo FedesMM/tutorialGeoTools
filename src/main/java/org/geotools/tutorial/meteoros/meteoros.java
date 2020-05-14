d9d1e6e935fba3f76725bca852bf5945fa36d2c3
FedesMM <fmatonte@gmail.com>
2020-05-14 16:32:51 -0300

    Initial commit

---
+++ src/main/java/org/geotools/tutorial/meteoros/meteoros.java
@@ -0,0 +1,224 @@
+package org.geotools.tutorial.meteoros;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.UIManager;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataUtilities;
+import org.geotools.data.DefaultTransaction;
+import org.geotools.data.Transaction;
+import org.geotools.data.collection.ListFeatureCollection;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.geotools.swing.data.JFileDataStoreChooser;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+
+public class meteoros {
+
+    public static void main(String[] args) throws Exception {
+        // Set cross-platform look & feel for compatability
+        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+
+        File file = JFileDataStoreChooser.showOpenFile("csv", null);
+        if (file == null) {
+            return;
+        }
+
+        /*
+         * We use the DataUtilities class to create a FeatureType that will describe the data in our
+         * shapefile.
+         *
+         * See also the createFeatureType method below for another, more flexible approach.
+         */
+        final SimpleFeatureType TYPE=createFeatureType();
+
+        /*
+         * A list to collect features as we create them.
+         */
+        List<SimpleFeature> features = new ArrayList<>();
+
+        /*
+         * GeometryFactory will be used to create the geometry attribute of each feature,
+         * using a Point object for the location.
+         */
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
+        SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(TYPE);
+
+        int minLon = -180;
+        int maxLon = +180;
+        int minLat = -90;
+        int maxLat = +90;
+
+        for (int iMeteoro = 0; iMeteoro < 100; iMeteoro++) {
+            double random_Lon = Math.random() * (maxLon - minLon + 1) + minLon;
+            double random_Lat = Math.random() * (maxLat - minLat + 1) + minLat;
+
+            String name ="Meteoro "+ (iMeteoro+1);
+            /* Longitude (= x coord) first ! */
+            Point point = geometryFactory.createPoint(new Coordinate(random_Lon, random_Lat));
+            Polygon polygon = (Polygon) point.buffer( 100 );
+            int number = iMeteoro+1;
+            //featureBuilder.add(point);
+            featureBuilder.add(polygon);
+            featureBuilder.add(name);
+            featureBuilder.add(number);
+            SimpleFeature feature = featureBuilder.buildFeature(null);
+            features.add(feature);
+        }
+
+        //ADD montevideo
+        double latMontevideo= -34.901112;
+        double lonMontevideo= -56.164532;
+        String name ="Montevideo";
+        int number = 101;
+        Point point = geometryFactory.createPoint(new Coordinate(latMontevideo, lonMontevideo));
+        Polygon polygon = (Polygon) point.buffer( 18 );
+
+        featureBuilder.add(polygon);
+        featureBuilder.add(name);
+        featureBuilder.add(number);
+        SimpleFeature feature = featureBuilder.buildFeature(null);
+        features.add(feature);
+
+
+
+        /*
+         * Get an output file name and create the new shapefile
+         */
+        File newFile = getNewShapeFile(file);
+
+        ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
+
+        Map<String, Serializable> params = new HashMap<>();
+        params.put("url", newFile.toURI().toURL());
+        params.put("create spatial index", Boolean.TRUE);
+
+        ShapefileDataStore newDataStore =
+                (ShapefileDataStore) dataStoreFactory.createNewDataStore(params);
+
+        /*
+         * TYPE is used as a template to describe the file contents
+         */
+        newDataStore.createSchema(TYPE);
+
+        /*
+         * Write the features to the shapefile
+         */
+        Transaction transaction = new DefaultTransaction("create");
+
+        String typeName = newDataStore.getTypeNames()[0];
+        SimpleFeatureSource featureSource = newDataStore.getFeatureSource(typeName);
+        SimpleFeatureType SHAPE_TYPE = featureSource.getSchema();
+        /*
+         * The Shapefile format has a couple limitations:
+         * - "the_geom" is always first, and used for the geometry attribute name
+         * - "the_geom" must be of type Point, MultiPoint, MuiltiLineString, MultiPolygon
+         * - Attribute names are limited in length
+         * - Not all data types are supported (example Timestamp represented as Date)
+         *
+         * Each data store has different limitations so check the resulting SimpleFeatureType.
+         */
+        System.out.println("SHAPE:" + SHAPE_TYPE);
+
+        if (featureSource instanceof SimpleFeatureStore) {
+            SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
+            /*
+             * SimpleFeatureStore has a method to add features from a
+             * SimpleFeatureCollection object, so we use the ListFeatureCollection
+             * class to wrap our list of features.
+             */
+            SimpleFeatureCollection collection = new ListFeatureCollection(TYPE, features);
+            featureStore.setTransaction(transaction);
+            try {
+                featureStore.addFeatures(collection);
+                transaction.commit();
+            } catch (Exception problem) {
+                problem.printStackTrace();
+                transaction.rollback();
+            } finally {
+                transaction.close();
+            }
+            System.exit(0); // success!
+        } else {
+            System.out.println(typeName + " does not support read/write access");
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Prompt the user for the name and path to use for the output shapefile
+     *
+     * @param csvFile the input csv file used to create a default shapefile name
+     * @return name and path for the shapefile as a new File object
+     */
+    private static File getNewShapeFile(File csvFile) {
+        String path = csvFile.getAbsolutePath();
+        String newPath = path.substring(0, path.length() - 4) + ".shp";
+
+        JFileDataStoreChooser chooser = new JFileDataStoreChooser("shp");
+        chooser.setDialogTitle("Save shapefile");
+        chooser.setSelectedFile(new File(newPath));
+
+        int returnVal = chooser.showSaveDialog(null);
+
+        if (returnVal != JFileDataStoreChooser.APPROVE_OPTION) {
+            // the user cancelled the dialog
+            System.exit(0);
+        }
+
+        File newFile = chooser.getSelectedFile();
+        if (newFile.equals(csvFile)) {
+            System.out.println("Error: cannot replace " + csvFile);
+            System.exit(0);
+        }
+
+        return newFile;
+    }
+
+    /**
+     * Here is how you can use a SimpleFeatureType builder to create the schema for your shapefile
+     * dynamically.
+     *
+     * <p>This method is an improvement on the code used in the main method above (where we used
+     * DataUtilities.createFeatureType) because we can set a Coordinate Reference System for the
+     * FeatureType and a a maximum field length for the 'name' field dddd
+     */
+    private static SimpleFeatureType createFeatureType() {
+
+        SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+        builder.setName("Location");
+        builder.setCRS(DefaultGeographicCRS.WGS84); // <- Coordinate reference system
+
+        // add attributes in order
+        builder.add("the_geom", Polygon.class);
+        builder.length(15).add("Name", String.class); // <- 15 chars width for name field
+        builder.add("number", Integer.class);
+
+        // build the type
+        final SimpleFeatureType LOCATION = builder.buildFeatureType();
+
+        return LOCATION;
+    }
+
+}
